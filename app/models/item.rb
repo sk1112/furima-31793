131ce4d62c133538e977c0d21b465bec19a2e53b
sk1112 <72970375+sk1112@users.noreply.github.com>
2020-11-20 14:56:21 +0900

    【修正】不要なファイルの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :category
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :delivery_day
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :delivery_fee
+  belongs_to :delivery_day
 
   with_options presence: true do
     validates :name
--- spec/factories/conditons.rb
+++
@@ -1,4 +0,0 @@
-FactoryBot.define do
-  factory :conditon do
-  end
-end
