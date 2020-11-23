ef4c8901158df7cd989162667cf66bae31ce6c35
sk1112 <72970375+sk1112@users.noreply.github.com>
2020-11-23 13:20:04 +0900

    【修正】edit.html.erbの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 
   def update
