ece8c36722c243aaa013066b2a5f2ce5cd83b995
sk1112 <72970375+sk1112@users.noreply.github.com>
2020-11-23 14:57:18 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless current_user.id == @item.user.id
   end
 
   def update
